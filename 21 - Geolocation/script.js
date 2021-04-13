3f349831828c023174b3f31b5b07ccc10ba44757
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-04-13 20:01:04 -0300

    Luara - JavaScript 21

--- 21 - Geolocation/script.js
+++ 21 - Geolocation/script.js
@@ -3,7 +3,7 @@ const speed = document.querySelector('.speed-value')
 
 navigator.geolocation.watchPosition((data) => {
     speed.textContent = data.coords.speed
-    arrow.getElementsByClassName.transform = `rotate(${data.coords.heading}deg)`
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
     console.err(err)
     alert('HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!!')
